da50f81dfb6096a7da61d62842e97c6dabb1ba11
Ot <otpratf@gmail.com>
2020-06-05 13:20:57 +0200

    Added get_scale_mm function to bouter __init__

--- bouter/__init__.py
+++ bouter/__init__.py
@@ -1,6 +1,7 @@
 from bouter.experiment import Experiment
 from bouter.embedded import EmbeddedExperiment
 from bouter.multisession_exp import MultiSessionExperiment
+import numpy as np
 
 
 def load_folder(folder, exp_class=Experiment):
@@ -14,3 +15,19 @@ def load_folder(folder, exp_class=Experiment):
     """
     folder = Path(folder)
     return [exp_class(f) for f in sorted(folder.glob("**/*_metadata.json"))]
+
+
+def get_scale_mm(exp_class: Experiment):
+    """ Return camera pixel size in millimeters
+
+    :param exp:
+    :return:
+    """
+    cal_params = exp_class["stimulus"]["calibration_params"]
+    if exp_class["general"]["animal"]["embedded"]:
+        return cal_params["mm_px"]
+    else:
+        proj_mat = np.array(cal_params["cam_to_proj"])
+        return (
+            np.linalg.norm(np.array([1.0, 0.0]) @ proj_mat[:, :2]) * cal_params["mm_px"]
+        )
