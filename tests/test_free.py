9811cf5ebd57caf6ad4c820e6fa8c2857e5f9e8d
Ot <otpratf@gmail.com>
2020-06-05 11:21:23 +0200

    Started working on tests for freely-swimming experiments

---
+++ tests/test_free.py
@@ -0,0 +1,10 @@
+import bouter
+from numpy.testing import assert_array_almost_equal
+from pathlib import Path
+import flammkuchen as fl
+import numpy as np
+
+from bouter import embedded
+
+
+ASSETS_PATH = Path(__file__).parent / "assets"
