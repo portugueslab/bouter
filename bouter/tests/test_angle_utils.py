c4e765ac9eb62e2184fbf252c3a95fa58a9750c9
vilim <vilim@neuro.mpg.de>
2020-08-17 16:17:59 +0200

    Re-added angle utilities

---
+++ bouter/tests/test_angle_utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+from bouter.angles import quantize_directions
+
+
+def test_quantize():
+    dirs, bins = quantize_directions(
+        np.array([0, -np.pi, np.pi, np.pi / 2 + 0.01])
+    )
+    np.testing.assert_array_equal(bins, [0, 4, 4, 2])
