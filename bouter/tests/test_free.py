3bc0d8d0ca25f3880cde90c475740c7b8f785556
vilim <online@vil.im>
2021-11-14 21:23:07 +0100

    regenerate & fix tests

--- bouter/tests/test_free.py
+++ bouter/tests/test_free.py
@@ -42,19 +42,13 @@ def test_tail_fixing_experiment(freely_swimming_exp_path):
     for i_fish in range(experiment.n_fish):
 
         # Load original dataframe:
-        original_fish_mat = experiment.behavior_log.loc[
-            :, experiment.tail_columns[i_fish]
-        ].values.copy()
+        original_fish_mat = experiment.behavior_log.loc[:, experiment.tail_columns[i_fish]].values.copy()
         original_mats.append(original_fish_mat)
 
         # Nan random points and substitute experiment data, in place:
-        corrupted_fish_mat = corrupt_tail_matrix(
-            original_fish_mat, prop_missing=0.3, std=1
-        )
+        corrupted_fish_mat = corrupt_tail_matrix(original_fish_mat, prop_missing=0.3, std=1)
         corrupted_mats.append(corrupted_fish_mat)
-        experiment.behavior_log.loc[
-            :, experiment.tail_columns[i_fish]
-        ] = corrupted_fish_mat
+        experiment.behavior_log.loc[:, experiment.tail_columns[i_fish]] = corrupted_fish_mat
 
     # Fix the experiment behavior_log, in place
     experiment.reconstruct_missing_segments(continue_curvature=4)
@@ -64,10 +58,7 @@ def test_tail_fixing_experiment(freely_swimming_exp_path):
 
         fixed_pts = np.isnan(corrupted_mats[i_fish])
         diff_mat = np.abs(
-            original_mats[i_fish]
-            - experiment.behavior_log.loc[
-                :, experiment.tail_columns[i_fish]
-            ].values
+            original_mats[i_fish] - experiment.behavior_log.loc[:, experiment.tail_columns[i_fish]].values
         )
 
         assert np.median(diff_mat[fixed_pts]) < 0.1
@@ -80,17 +71,11 @@ def test_tail_fixing_revert(freely_swimming_exp_path):
     for i_fish in range(experiment.n_fish):
 
         # Load original dataframe:
-        original_fish_mat = experiment.behavior_log.loc[
-            :, experiment.tail_columns[i_fish]
-        ].values.copy()
+        original_fish_mat = experiment.behavior_log.loc[:, experiment.tail_columns[i_fish]].values.copy()
 
         # Nan random points and substitute experiment data, in place:
-        corrupted_fish_mat = corrupt_tail_matrix(
-            original_fish_mat, prop_missing=0.3, std=1
-        )
-        experiment.behavior_log.loc[
-            :, experiment.tail_columns[i_fish]
-        ] = corrupted_fish_mat
+        corrupted_fish_mat = corrupt_tail_matrix(original_fish_mat, prop_missing=0.3, std=1)
+        experiment.behavior_log.loc[:, experiment.tail_columns[i_fish]] = corrupted_fish_mat
 
     corrupted_behavior_log = experiment.behavior_log
 
@@ -106,40 +91,40 @@ def test_tail_fixing_revert(freely_swimming_exp_path):
 def test_compute_velocity(freely_swimming_exp_path):
     experiment = free.FreelySwimmingExperiment(freely_swimming_exp_path)
     velocities_df = experiment.compute_velocity()
-    fish_vels = velocities_df[
-        ["vel2_f{}".format(i_fish) for i_fish in range(experiment.n_fish)]
-    ]
+    fish_vels = velocities_df[["vel_f{}".format(i_fish) for i_fish in range(experiment.n_fish)]]
 
     # Load computed velocities
-    loaded_vel2 = fl.load(
+    loaded_vel = fl.load(
         ASSETS_PATH / "freely_swimming_dataset" / "test_extracted_bouts.h5",
         "/velocities",
     )
 
     # Compare DataFrame with velocities from the 3 experiment fish
-    assert_frame_equal(fish_vels, loaded_vel2)
+    assert_frame_equal(fish_vels, loaded_vel)
 
 
 def test_bout_extraction(freely_swimming_exp_path):
     experiment = free.FreelySwimmingExperiment(freely_swimming_exp_path)
-    bouts, cont = experiment.get_bouts()
+    bouts, continuities = experiment.get_bouts()
 
     # Load expected bouts to be extracted. Only first fish is used for the assertion
     loaded_bouts = fl.load(
         ASSETS_PATH / "freely_swimming_dataset" / "test_extracted_bouts.h5",
         "/bouts",
     )
-    loaded_cont = fl.load(
+    loaded_continuities = fl.load(
         ASSETS_PATH / "freely_swimming_dataset" / "test_extracted_bouts.h5",
         "/continuity",
     )
 
     # Compare dataframes for each of the detected bouts in the first fish
-    for bout in range(len(bouts[0])):
-        assert_frame_equal(bouts[0][bout], loaded_bouts[bout])
+    for fish_bouts, fish_bouts_loaded in zip(bouts, loaded_bouts):
+        for bouts, loaded_bouts in zip(fish_bouts, fish_bouts_loaded):
+            assert_frame_equal(bouts, loaded_bouts)
 
     # Compare also continuity array
-    assert_array_equal(cont[0], loaded_cont)
+    for cont, loaded_cont in zip(continuities, loaded_continuities):
+        assert_array_equal(cont, loaded_cont)
 
 
 def test_bout_summary(freely_swimming_exp_path):
