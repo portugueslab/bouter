6d750f1b7a87528cc90a3812833974212ac7c4e0
vilim <online@vil.im>
2021-11-28 19:54:11 +0100

    picky linter

--- bouter/tests/test_nwb_conversion.py
+++ bouter/tests/test_nwb_conversion.py
@@ -8,10 +8,10 @@ from bouter.nwb.conversion import experiment_to_nwb
 def test_convert_freely_swimming(freely_swimming_exp_path):
     exp = free.FreelySwimmingExperiment(freely_swimming_exp_path)
     tempdir = tempfile.mkdtemp()
-    nwb_exp = experiment_to_nwb(exp, Path(tempdir) / "free.nwb")
+    experiment_to_nwb(exp, Path(tempdir) / "free.nwb")
 
 
 def test_convert_embedded(embedded_exp_path):
     exp = embedded.EmbeddedExperiment(embedded_exp_path)
     tempdir = tempfile.mkdtemp()
-    nwb_exp = experiment_to_nwb(exp, Path(tempdir) / "embedded.nwb")
+    experiment_to_nwb(exp, Path(tempdir) / "embedded.nwb")
