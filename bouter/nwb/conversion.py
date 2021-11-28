4fb05db2687a08a15eded67fb1d46c4c02ec4468
vilim <online@vil.im>
2021-11-28 18:58:40 +0100

    free and tests

--- bouter/nwb/conversion.py
+++ bouter/nwb/conversion.py
@@ -6,12 +6,16 @@ from pathlib import Path
 
 import numpy as np
 import pynwb.file
-from pynwb import NWBFile, NWBHDF5IO, TimeSeries
+from pynwb import NWBFile, NWBHDF5IO
 from pynwb.misc import AbstractFeatureSeries
 
 from bouter import Experiment
+from bouter import df_utilities
 from bouter.embedded import EmbeddedExperiment
 from bouter.free import FreelySwimmingExperiment
+
+REFERENCE_FRAME_DESCRIPTION = "left-handed coordinate system with the x axis pointing rightward in the camera view"
+TAIL_SHAPE_REFERENCE_FRAME_DESCRIPTION = "relative tail angles"
 
 
 def _get_subject_metadata(exp: Experiment):
@@ -23,7 +27,7 @@ def _get_subject_metadata(exp: Experiment):
         description=exp["general"]["animal"]["comments"],
         genotype=exp["general"]["animal"]["genotype"],
         species=exp["general"]["animal"]["species"],
-        subject_id=exp["general"]["animal"]["id"],
+        subject_id=str(exp["general"]["animal"]["id"]),
     )
 
 
@@ -102,23 +106,51 @@ def _save_behavior(exp: Experiment, nwbfile: NWBFile):
 
 @_save_behavior.register
 def _(exp: FreelySwimmingExperiment, nwbfile: NWBFile):
-    raise NotImplementedError(
-        "Freely-swimming experiment serialization not yet implemented"
-    )
+    for i_fish in range(exp.n_fish):
+        fish_data = exp._rename_fish(
+            exp.behavior_log, i_fish, exp.n_tail_segments
+        )
+        tail_direction = pynwb.behavior.SpatialSeries(
+            "fish_direction",
+            fish_data.loc[:, "theta"].values,
+            timestamps=fish_data.t.values,
+            reference_frame=REFERENCE_FRAME_DESCRIPTION,
+        )
+        tail_shape = pynwb.behavior.SpatialSeries(
+            "tail_shape",
+            fish_data.loc[
+                :, df_utilities.tail_column_names(exp.n_tail_segments)
+            ].values,
+            timestamps=tail_direction,
+            unit="radians",
+            reference_frame=TAIL_SHAPE_REFERENCE_FRAME_DESCRIPTION,
+        )
+        position = pynwb.behavior.SpatialSeries(
+            "fish_position",
+            fish_data.loc[:, ["x", "y"]].values * exp.camera_px_in_mm,
+            timestamps=tail_direction,
+            unit="mm",
+            reference_frame=REFERENCE_FRAME_DESCRIPTION,
+        )
+        per_fish_data = pynwb.behavior.Position(
+            [position, tail_direction, tail_shape], name=f"fish_{i_fish}"
+        )
+        nwbfile.add_acquisition(per_fish_data)
 
 
 @_save_behavior.register
 def _(exp: EmbeddedExperiment, nwbfile: NWBFile):
-    tail_shapes = TimeSeries(
+    tail_shapes = pynwb.behavior.SpatialSeries(
         "tail_shape",
         timestamps=exp.behavior_log.t.values,
         data=exp.behavior_log.loc[:, exp.tail_columns].values,
         unit="radians",
+        reference_frame=TAIL_SHAPE_REFERENCE_FRAME_DESCRIPTION,
     )
     nwbfile.add_acquisition(tail_shapes)
 
 
-def experiment_to_nwb(exp: Experiment, nwb_path: Path):
+def experiment_to_nwb(exp: Experiment, nwb_path: Path) -> pynwb.NWBFile:
     """Convert a bouter Experiment into a nwb file."""
     nwbfile = NWBFile(
         session_description="demonstrate adding to an NWB file",
@@ -131,8 +163,9 @@ def experiment_to_nwb(exp: Experiment, nwb_path: Path):
     )
 
     _save_stimulus(exp, nwbfile)
-
-    nwbfile.save()
+    _save_behavior(exp, nwbfile)
 
     with NWBHDF5IO(nwb_path, "w") as io:
         io.write(nwbfile)
+
+    return nwbfile
